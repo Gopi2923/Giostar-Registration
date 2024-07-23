d68b13f9ba3b3f44c011a0bea1bdf6f1e05b8a91
Gopi2923 <dgopidhikshan@gmail.com>
2024-07-23 17:10:29 +0530

    Added icon and styles.

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './HomePage.css'
 import logo from '../../assets/logo-01.png'
 import { useNavigate } from 'react-router-dom'
-import backgroundimg from '../../assets/background-image.jpg'
 
 const HomePage = () => {
    const navigate = useNavigate()
@@ -15,7 +14,7 @@ const HomePage = () => {
     <div className='home-page'>
       <div className='content'>
        <img src={logo} alt="Hospital Logo" className='logo'/>
-        <h1 className="title">Effortless Patient Registration</h1>
+        <h4 className="title">Effortless Patient Registration</h4>
         <p className="subtitle">"From Registration to Care: Enhancing the Patient Journey"</p>
        <button className='register-btn' onClick={handleButtonClick}>Register</button>
        </div>
