509790330d25f62e2b017a27d12f9384876935ec
Gopi2923 <dgopidhikshan@gmail.com>
2024-07-23 16:41:07 +0530

    Added background image and logo icon.

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './HomePage.css'
 import logo from '../../assets/logo-01.png'
 import { useNavigate } from 'react-router-dom'
+import backgroundimg from '../../assets/background-image.jpg'
 
 const HomePage = () => {
    const navigate = useNavigate()
@@ -12,8 +13,13 @@ const HomePage = () => {
 
   return (
     <div className='home-page'>
+      <div className='content'>
        <img src={logo} alt="Hospital Logo" className='logo'/>
+        <h1 className="title">Effortless Patient Registration</h1>
+        <p className="subtitle">"From Registration to Care: Enhancing the Patient Journey"</p>
        <button className='register-btn' onClick={handleButtonClick}>Register</button>
+       </div>
+       <div className='background-img'></div>
     </div>
   )
 }
