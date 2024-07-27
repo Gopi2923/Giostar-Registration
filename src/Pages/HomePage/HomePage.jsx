25570cd89b7cda0e51982746e4db913bbbd7c09b
Gopi2923 <dgopidhikshan@gmail.com>
2024-07-27 12:41:45 +0530

    added toast messages.

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -9,6 +9,8 @@ import { TailSpin } from 'react-loader-spinner';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { format } from 'date-fns';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -16,7 +18,6 @@ const HomePage = () => {
   const [filteredRegistrations, setFilteredRegistrations] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [selectedDate, setSelectedDate] = useState(null);
-  const [error, setError] = useState(null);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const csvLinkRef = useRef(null);
 
@@ -26,30 +27,35 @@ const HomePage = () => {
 
   const fetchRegistrations = async () => {
     if (!selectedDate) {
-      alert("Please select a date.");
+      toast.error("Please select a date.");
       return;
     }
 
     setIsLoading(true);
-    setError(null);
     try {
       const formattedDate = format(selectedDate, 'yyyy/MM/dd');
       const response = await axios.post('https://giostar.onrender.com/registration/getAll', {
         createdAt: formattedDate
       });
-      const data = response.data.data; // assuming response.data.data contains the array of registrations
-      
-      // Filter out unwanted columns here
-      const filteredData = data.map(({ _id, isRegistered, updatedAt, __v, address, city, state, pincode, reason, typeOfVisit, middleName, lastname, ...rest }) => rest);
 
-      setRegistrations(filteredData);
-      setFilteredRegistrations(filteredData);
-      setModalIsOpen(false); // Close the modal
+      const data = response.data.data;
+
+      // Check if data is empty and show toast message
+      if (!data || data.length === 0) {
+        toast.error(response.data.message || 'Empty Registrations');
+      } else {
+        const filteredData = data.map(({ _id, isRegistered, updatedAt, __v, address, city, state, pincode, reason, typeOfVisit, middleName, lastname, ...rest }) => rest);
+        setRegistrations(filteredData);
+        setFilteredRegistrations(filteredData);
+        setModalIsOpen(false); // Close the modal
+      }
     } catch (error) {
-      setError('Error fetching registrations');
+      const errorMessage = error.response?.data?.message || 'Error fetching registrations';
+      toast.error(errorMessage);
       console.error('Error fetching registrations:', error);
     } finally {
       setIsLoading(false);
+      setSelectedDate(null);
     }
   };
 
@@ -59,14 +65,18 @@ const HomePage = () => {
     }
   }, [filteredRegistrations]);
 
-  // Define CSV headers
+  const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString('en-GB', options);
+  };
+  
   const csvHeaders = [
     { label: 'Name', key: 'firstName' },
     { label: 'Age', key: 'age' },
     { label: 'Gender', key: 'gender' },
     { label: 'Email', key: 'email' },
     { label: 'Mobile Number', key: 'mobile_number' },
-    { label: 'Date of Registration', key: 'createdAt' }, // Adjusted to use the 'createdAt' key
+    { label: 'Date of Registration', key: 'createdAt' },
     { label: 'Patient ID', key: 'patientId' },
   ];
 
@@ -78,7 +88,13 @@ const HomePage = () => {
         <p className="subtitle">"From Registration to Care: Enhancing the Patient Journey"</p>
         <button className="register-btn" onClick={handleButtonClick}>Register</button>
         <button className="export-btn" onClick={() => setModalIsOpen(true)}>
-          {isLoading ? <> Loading <TailSpin color="#fff" height={34} width={44}/> </> : <><FaDownload /> Export Registrations</>}
+          {isLoading ? (
+            <div className="spinner-container">
+              Loading <TailSpin color="#fff" height={24} width={24} />
+            </div>
+          ) : (
+            <><FaDownload /> Export Registrations</>
+          )}
         </button>
         <CSVLink
           data={filteredRegistrations}
@@ -100,11 +116,10 @@ const HomePage = () => {
               onChange={date => setSelectedDate(date)}
               placeholderText="Select a date"
               dateFormat="yyyy/MM/dd"
-              className="date-input"
             />
           </div>
           <button className="download-btn" onClick={fetchRegistrations} disabled={isLoading}>
-            {isLoading ? (
+          {isLoading ? (
               <div className="spinner-container">
                 Loading <TailSpin color="#fff" height={24} width={24} />
               </div>
@@ -114,6 +129,8 @@ const HomePage = () => {
           </button>
         </div>
       </div>
+
+      <ToastContainer />
     </div>
   );
 };
