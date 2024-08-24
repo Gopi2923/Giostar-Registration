530e58a9684c4538bab4568f8b787d94fec36860
Gopi2923 <dgopidhikshan@gmail.com>
2024-08-24 12:03:04 +0530

    modal changes.

--- src/Pages/FollowUp/Modal.jsx
+++ src/Pages/FollowUp/Modal.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import Modal from 'react-modal';
 
 const FollowUpConsultationModal = ({ isOpen, onClose, onSelect }) => {
+    if (!isOpen) return null;
+
     return (
-        
-        <Modal isOpen={isOpen} onRequestClose={onClose}>
-            <div className="modal">
-        <div className="modal-content">
-          <span className="close" onClick={onClose}>&times;</span>
-            <h2>Select an Option</h2>
-            <div className="button-containers">
-            <button onClick={() => onSelect('follow-up')} className='modal-content-btn'>Follow-Up</button>
-            <button onClick={() => onSelect('consultation')} className='modal-content-btn'>Consultation</button>
-            </div>
+        <div className="modal">
+            <div className="modal-overlay" onClick={onClose}></div>
+            <div className="modal-content">
+                <span className="close" onClick={onClose}>&times;</span>
+                <h2>Select an Option</h2>
+                <div className="button-containers">
+                    <button onClick={() => onSelect('follow-up')} className='modal-content-btn'>Follow-Up</button>
+                    <button onClick={() => onSelect('consultation')} className='modal-content-btn'>Consultation</button>
+                </div>
             </div>
         </div>
-        </Modal>
-        
     );
 };
 
